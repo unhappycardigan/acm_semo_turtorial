192678275bec69126d0987d430b1dcde12120a10
unhappycardigan <32884593+unhappycardigan@users.noreply.github.com>
2018-01-29 18:49:02 -0600

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+int add(int x, int y);
+
 int main()
 {
 	std::cout << "Hello World!\n";
 	std::cout << "a =" << "a\n";
 	
 	return 0;
+}
+int add(intx, int y) {
+	return x+y;
 }
